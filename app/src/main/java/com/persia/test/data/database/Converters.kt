47b116989c99a1fddf473d36f9f010a43450bd0e
alireza <alireza.mastery@gmail.com>
2022-09-19 21:14:22 +0430

    added type converters

--- app/src/main/java/com/persia/test/data/database/Converters.kt
+++ app/src/main/java/com/persia/test/data/database/Converters.kt
@@ -1,9 +1,19 @@
 package com.persia.test.data.database
 
+import androidx.room.ProvidedTypeConverter
 import androidx.room.TypeConverter
+import com.persia.test.data.domain.models.Variant
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import java.util.*
 
-class Converters {
+
+@ProvidedTypeConverter
+class Converters() {
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
 
     @TypeConverter
     fun fromTimestamp(value: Long?): Date? {
@@ -13,5 +23,15 @@ class Converters {
     @TypeConverter
     fun dateToTimestamp(date: Date?): Long? {
         return date?.time?.toLong()
+    }
+
+    @TypeConverter
+    fun fromVariantSelector(data: Variant.VariantSelector): String {
+        return moshi.adapter(Variant.VariantSelector::class.java).toJson(data)
+    }
+
+    @TypeConverter
+    fun toVariantSelector(json: String): Variant.VariantSelector {
+        return moshi.adapter(Variant.VariantSelector::class.java).fromJson(json)!!
     }
 }
